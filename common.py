731b7405b0ce02e37a6325818e45ca9fbc5aafa6
keaising <keaising@gmail.com>
2022-12-29 20:34:01 +0800

    update for new python version

--- common.py
+++ common.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 import argparse
-import datetime
 import webbrowser
 from datetime import datetime, timedelta
 from email.utils import parsedate_tz
-
 import twitter
 import yaml
 from requests_oauthlib import OAuth1Session
 from twitter import TwitterError
 
-REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
-ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
-AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
-SIGNIN_URL = 'https://api.twitter.com/oauth/authenticate'
+REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
+ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
+AUTHORIZATION_URL = "https://api.twitter.com/oauth/authorize"
+SIGNIN_URL = "https://api.twitter.com/oauth/authenticate"
 
 
 def confirm(message, default=None):
@@ -21,63 +19,70 @@ def confirm(message, default=None):
         t = input(message)
     else:
         if default:
-            default_input = 'y'
+            default_input = "y"
         else:
-            default_input = 'n'
-        t = input('%s [%s]' % (message, default_input))
-    if t == '' or t is None and default is not None:
+            default_input = "n"
+        t = input("%s [%s]" % (message, default_input))
+    if t == "" or t is None and default is not None:
         return default
-    while t != 'y' and t != 'n':
-        t = input('Type y or n: ')
-    if t == 'y':
+    while t != "y" and t != "n":
+        t = input("Type y or n: ")
+    if t == "y":
         return True
     else:
         return False
 
 
 def get_access_token(ck, cs):
-    oauth_client = OAuth1Session(client_key=ck, client_secret=cs, callback_uri='oob')
+    oauth_client = OAuth1Session(client_key=ck, client_secret=cs, callback_uri="oob")
 
-    print('\nRequesting temp token from Twitter...\n')
+    print("\nRequesting temp token from Twitter...\n")
 
     try:
         resp = oauth_client.fetch_request_token(REQUEST_TOKEN_URL)
     except ValueError as e:
-        raise 'Invalid response from Twitter requesting temp token: {0}'.format(e)
+        raise Exception("Invalid response from Twitter requesting temp token: ") from e
 
     url = oauth_client.authorization_url(AUTHORIZATION_URL)
 
-    print('I will try to start a browser to visit the following Twitter page '
-          'if a browser will not start, copy the URL to your browser '
-          'and retrieve the pincode to be used '
-          'in the next step to obtaining an Authentication Token: \n'
-          '\n\t{0}'.format(url))
+    print(
+        "I will try to start a browser to visit the following Twitter page "
+        "if a browser will not start, copy the URL to your browser "
+        "and retrieve the pincode to be used "
+        "in the next step to obtaining an Authentication Token: \n"
+        "\n\t{0}".format(url)
+    )
 
     webbrowser.open(url)
-    pincode = input('\nEnter your pincode? ')
+    pincode = input("\nEnter your pincode? ")
 
-    print('\nGenerating and signing request for an access token...\n')
+    print("\nGenerating and signing request for an access token...\n")
 
-    oauth_client = OAuth1Session(client_key=ck, client_secret=cs,
-                                 resource_owner_key=resp.get('oauth_token'),
-                                 resource_owner_secret=resp.get('oauth_token_secret'),
-                                 verifier=pincode)
+    oauth_client = OAuth1Session(
+        client_key=ck,
+        client_secret=cs,
+        resource_owner_key=resp.get("oauth_token"),
+        resource_owner_secret=resp.get("oauth_token_secret"),
+        verifier=pincode,
+    )
     try:
         resp = oauth_client.fetch_access_token(ACCESS_TOKEN_URL)
     except ValueError as e:
-        raise 'Invalid response from Twitter requesting temp token: {0}'.format(e)
+        raise Exception("Invalid response from Twitter requesting temp token: ") from e
 
-    return resp.get('oauth_token'), resp.get('oauth_token_secret')
+    return resp.get("oauth_token"), resp.get("oauth_token_secret")
 
 
 def load_credentials():
     try:
-        with open('.twitter_credentials.yml') as f:
-            c = yaml.load(f)
-            return {'ck': c['consumer_key'],
-                    'cs': c['consumer_secret'],
-                    'at': c['access_token'],
-                    'ats': c['access_token_secret']}
+        with open(".twitter_credentials.yml") as f:
+            c = yaml.load(f, Loader=yaml.FullLoader)
+            return {
+                "ck": c["consumer_key"],
+                "cs": c["consumer_secret"],
+                "at": c["access_token"],
+                "ats": c["access_token_secret"],
+            }
     except IOError:
         return None
 
@@ -85,49 +90,58 @@ def load_credentials():
 def get_credentials(crds):
     ck = None
     cs = None
-    if crds and crds['ck'] and crds['cs']:
-        if not confirm('Do you need to use new consumer key/secret?', default=False):
-            ck = crds['ck']
-            cs = crds['cs']
+    if crds and crds["ck"] and crds["cs"]:
+        if not confirm("Do you need to use new consumer key/secret?", default=False):
+            ck = crds["ck"]
+            cs = crds["cs"]
     if ck is None or cs is None:
-        ck = input('Input consumer key: ')
-        cs = input('Input consumer secret: ')
+        ck = input("Input consumer key: ")
+        cs = input("Input consumer secret: ")
 
-    if confirm('Do you have access token and secret already?', default=False):
-        at = input('Input access token: ')
-        ats = input('Input access token secret: ')
+    if confirm("Do you have access token and secret already?", default=False):
+        at = input("Input access token: ")
+        ats = input("Input access token secret: ")
     else:
         at, ats = get_access_token(ck, cs)
-    return {'ck': ck, 'cs': cs, 'at': at, 'ats': ats}
+    return {"ck": ck, "cs": cs, "at": at, "ats": ats}
 
 
 def api():
-    a, me = api2()
+    a, _ = api2()
     return a
 
 
 def api2():
     credentials = load_credentials()
 
-    if not credentials or confirm('Do you want to switch to a new user?', default=False):
+    if not credentials or confirm(
+        "Do you want to switch to a new user?", default=False
+    ):
         credentials = get_credentials(credentials)
-        with open('.twitter_credentials.yml', 'w') as f:
-            yaml.dump({
-                'consumer_key': credentials['ck'],
-                'consumer_secret': credentials['cs'],
-                'access_token': credentials['at'],
-                'access_token_secret': credentials['ats'],
-            }, f, default_flow_style=False)
+        with open(".twitter_credentials.yml", "w") as f:
+            yaml.dump(
+                {
+                    "consumer_key": credentials["ck"],
+                    "consumer_secret": credentials["cs"],
+                    "access_token": credentials["at"],
+                    "access_token_secret": credentials["ats"],
+                },
+                f,
+                default_flow_style=False,
+            )
 
-    a = twitter.Api(consumer_key=credentials['ck'], consumer_secret=credentials['cs'],
-                    access_token_key=credentials['at'],
-                    access_token_secret=credentials['ats'],
-                    sleep_on_rate_limit=True)
+    a = twitter.Api(
+        consumer_key=credentials["ck"],
+        consumer_secret=credentials["cs"],
+        access_token_key=credentials["at"],
+        access_token_secret=credentials["ats"],
+        sleep_on_rate_limit=True,
+    )
 
     try:
         me = a.VerifyCredentials()
     except TwitterError:
-        print('User logged out')
+        print("User logged out")
         exit(0)
 
     return a, me
@@ -135,12 +149,17 @@ def api2():
 
 def argparse_date(date_str):
     try:
-        return datetime.strptime(date_str, '%Y-%m-%d')
+        return datetime.strptime(date_str, "%Y-%m-%d")
     except ValueError:
-        raise argparse.ArgumentTypeError(f'Unrecognized date {date_str}')
+        raise argparse.ArgumentTypeError(f"Unrecognized date {date_str}")
 
 
 def to_datetime(datestring):
     time_tuple = parsedate_tz(datestring.strip())
+    if time_tuple is None:
+        return 0
     dt = datetime(*time_tuple[:6])
-    return dt - timedelta(seconds=time_tuple[-1])
+    sec = time_tuple[-1]
+    if sec is None:
+        return 0
+    return dt - timedelta(seconds=sec)
--- friends_backup.py
+++ friends_backup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from openpyxl import Workbook
-
+import xlsxwriter
 import common
 
 api = common.api()
@@ -15,10 +14,13 @@ while friends_cursor != 0:
     friends += ids
 print('You have %d followings' % len(friends))
 
-wb = Workbook()
-ws = wb.active
 
-for user in friends:
-    ws.append([user.id, '@%s' % user.screen_name, user.name, 'https://twitter.com/%s' % user.screen_name])
+workbook = xlsxwriter.Workbook('Following.xlsx')
+worksheet = workbook.add_worksheet("Following")
 
-wb.save('friends.xlsx')
+for i, user in enumerate(friends):
+    row = [user.id, '@%s' % user.screen_name, user.name, 'https://twitter.com/%s' % user.screen_name]
+    for j, item in enumerate(row):
+        worksheet.write(i, j, item)
+
+workbook.close()
